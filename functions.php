621e6526cbcb5ed55a39b43279e4fed230c419c0
alex-wdmg <wdmg.com.ua@gmail.com>
2018-08-27 02:29:27 +0300

    Register sidebar`s

--- functions.php
+++ functions.php
@@ -14,6 +14,7 @@ $author = "Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>";
 $shortname = "wp";
 $version = '1.0.0';
 
+	load_theme_textdomain('wp-bootstrap', get_template_directory().'/languages');
 // Load theme languages
 function load_theme_langs() {
 	load_theme_textdomain('wp-bootstrap', get_template_directory().'/languages');
@@ -29,5 +30,79 @@ function admin_assets() {
 	wp_enqueue_script('admin-js', get_template_directory_uri().'/assets/js/admin.js', null, null, true);
 }
 add_action('admin_head', 'admin_assets');
+
+
+
+
+
+
+
+// Register Sidebar`s
+function register_theme_sidebars() {
+	register_sidebar(
+		array(
+			'name' => __('Header Sidebar', 'wp-bootstrap'),
+			'id' => 'header-sidebar',
+			'before_widget' => '',
+			'after_widget' => '',
+			'before_title' => '',
+			'after_title' => '',
+		)
+	);
+	register_sidebar(
+		array(
+		'name' => __('Right Sidebar', 'wp-bootstrap'),
+		'id' => 'right-sidebar',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<div class="caption">',
+		'after_title' => '</div>',
+	));
+	register_sidebar(
+		array(
+		'name' => __('Left Sidebar', 'wp-bootstrap'),
+		'id' => 'left-sidebar',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<div class="caption">',
+		'after_title' => '</div>',
+	));
+	register_sidebar(
+		array(
+		'name' => __('Before Content', 'wp-bootstrap'),
+		'id' => 'before-content',
+		'before_widget' => '',
+		'after_widget' => '',
+		'before_title' => '<h3>',
+		'after_title' => '</h3>',
+	));
+	register_sidebar(
+		array(
+		'name' => __('After Content', 'wp-bootstrap'),
+		'id' => 'after-content',
+		'before_widget' => '',
+		'after_widget' => '',
+		'before_title' => '<h3>',
+		'after_title' => '</h3>',
+	));
+	register_sidebar(
+		array(
+			'name' => __('Footer Sidebar', 'wp-bootstrap'),
+			'id' => 'footer-sidebar',
+			'before_widget' => '',
+			'after_widget' => '',
+			'before_title' => '',
+			'after_title' => '',
+		)
+	);
+}
+add_action('widgets_init', 'register_theme_sidebars');
+
+
+
+
+
+
+
 
 ?>
