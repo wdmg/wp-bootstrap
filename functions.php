80c8e0e248c0b8b24837a87365c186864ca6ffb2
alex-wdmg <wdmg.com.ua@gmail.com>
2018-08-26 16:42:03 +0300

    Added theme information

--- functions.php
+++ functions.php
@@ -7,7 +7,12 @@
  * @since 1.0
  */
 
-
+// Theme Information
+$themename = "WoodPlast";
+$developer_uri = "http://wdmg.com.ua/wp-bootstrap/";
+$author = "Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>";
+$shortname = "wp";
+$version = '1.0.0';
 
 // Load theme languages
 function load_theme_langs() {
@@ -18,12 +23,11 @@ add_action('after_setup_theme', 'load_theme_langs');
 // Include theme-options.php for admin theme settings
 include 'theme-options.php';
 
-// Enqueue admin assets
+// Enqueue admin assets (only for console)
 function admin_assets() {
 	wp_enqueue_style('admin-styles', get_stylesheet_directory_uri().'/assets/css/admin.css', null, null);
 	wp_enqueue_script('admin-js', get_template_directory_uri().'/assets/js/admin.js', null, null, true);
 }
 add_action('admin_head', 'admin_assets');
-
 
 ?>
