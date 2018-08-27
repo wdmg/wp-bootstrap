00085e32162abc15dfebd989da9f13970be7f48b
alex-wdmg <wdmg.com.ua@gmail.com>
2018-08-27 18:09:55 +0300

    Add theme options subpage items

--- theme-options.php
+++ theme-options.php
@@ -9,21 +9,52 @@
  */
 
 // Manage theme options page
-function wp_bootstrap_options() {
+function wp_bootstrap_options_callback() {
 
-	global $themename, $shortname, $options;
+	global $themename, $shortname, $options, $current_screen;
 	if (!current_user_can('manage_options'))
 		wp_die('You do not have sufficient permissions to access this page.');
 
-	if($_REQUEST['action'] == 'save')
+	$request = false;
+	if(isset($_REQUEST["action"])) {
+		$request['action'] = urldecode($_REQUEST['action']);
+	} else {
+		$request['action'] = false;
+	}
+	if(isset($_REQUEST["section"])) {
+		$request['section'] = urldecode($_REQUEST['section']);
+	} else {
+		$request['section'] = trim(strstr($current_screen->id, '_page_'), '_page_');
+	}
+
+	// Header of theme options
+	echo '<div class="wrap">';
+	echo '<h1 class="wp-heading-inline">'. get_admin_page_title() .'<span class="title-count theme-count">'.$request['section'].'</span></h1>';
+	echo '<hr/>';
+
+	if($request['action'] == 'save')
 		echo '<div class="notice notice-success inline wp-pp-notice"><p>Настройки темы <strong>'. $themename .'</strong> были успешно сохранены!</p></div>';
 
-	if($_REQUEST['reset'])
+	if($request['action'] == 'reset')
 		echo '<div class="notice notice-error inline wp-pp-notice"><p>Настройки темы <strong>'. $themename .'</strong> были сброшены!</p></div>';
 
-	if($_REQUEST['restore'])
+	if($request['action'] == 'restore')
 		echo '<div class="notice notice-warning inline wp-pp-notice"><p>Настройки темы <strong>'. $themename .'</strong> были восстановлены!</p></div>';
 
+	// Theme options functionality
+	if($request['section'] == 'head-body-options') {
+
+	} else if($request['section'] == 'sidebar-menu-options') {
+
+	} else if($request['section'] == 'backup-restore-options') {
+
+	} else if($request['section'] == 'theme-credits') {
+
+	} else { // default section: 'theme-options'
+
+	}
+
+	echo '</div>';
 }
 
 // Only admin actions
@@ -60,9 +91,41 @@ if (is_admin()) {
 			__('Theme options', 'wp-bootstrap'),
 			'manage_options',
 			'theme-options',
-			'wp_bootstrap_options',
+			'wp_bootstrap_options_callback',
 			'dashicons-welcome-widgets-menus',
 			62 // Default position order for appearance menu + 2
+		);
+		add_submenu_page(
+			'theme-options',
+			__('Head & Body options', 'wp-bootstrap'),
+			__('Head & Body', 'wp-bootstrap'),
+			'manage_options',
+			'head-body-options',
+			'wp_bootstrap_options_callback'
+		);
+		add_submenu_page(
+			'theme-options',
+			__('Sidebar & Menu options', 'wp-bootstrap'),
+			__('Sidebar & Menu', 'wp-bootstrap'),
+			'manage_options',
+			'sidebar-menu-options',
+			'wp_bootstrap_options_callback'
+		);
+		add_submenu_page(
+			'theme-options',
+			__('BackUp & Restore options', 'wp-bootstrap'),
+			__('BackUp & Restore', 'wp-bootstrap'),
+			'manage_options',
+			'backup-restore-options',
+			'wp_bootstrap_options_callback'
+		);
+		add_submenu_page(
+			'theme-options',
+			__('Credits theme', 'wp-bootstrap'),
+			__('Credits', 'wp-bootstrap'),
+			'manage_options',
+			'theme-credits',
+			'wp_bootstrap_options_callback'
 		);
     });
 
