26fdad461469727464b89910c57e3c7f6a583321
alex-wdmg <wdmg.com.ua@gmail.com>
2018-08-27 16:38:22 +0300

    Added theme options page, theme options menu

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
  */
 
 // Theme Information
-$themename = "WoodPlast";
+$themename = "WP Bootstrap";
 $developer_uri = "http://wdmg.com.ua/wp-bootstrap/";
 $author = "Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>";
 $shortname = "wp";
@@ -31,82 +31,102 @@ function admin_assets() {
 add_action('admin_head', 'admin_assets');
 
 
-
-
-
-
-
 // Register Sidebar`s
 add_action('widgets_init', function() {
-	register_sidebar(
-		array(
-			'name' => __('Header Sidebar', 'wp-bootstrap'),
-			'id' => 'header-sidebar',
-			'before_widget' => '',
-			'after_widget' => '',
-			'before_title' => '',
-			'after_title' => '',
-		)
-	);
-	register_sidebar(
-		array(
-		'name' => __('Right Sidebar', 'wp-bootstrap'),
-		'id' => 'right-sidebar',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget' => '</aside>',
-		'before_title' => '<div class="caption">',
-		'after_title' => '</div>',
-	));
-	register_sidebar(
-		array(
-		'name' => __('Left Sidebar', 'wp-bootstrap'),
-		'id' => 'left-sidebar',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget' => '</aside>',
-		'before_title' => '<div class="caption">',
-		'after_title' => '</div>',
-	));
-	register_sidebar(
-		array(
-		'name' => __('Before Content', 'wp-bootstrap'),
-		'id' => 'before-content',
-		'before_widget' => '',
-		'after_widget' => '',
-		'before_title' => '<h3>',
-		'after_title' => '</h3>',
-	));
-	register_sidebar(
-		array(
-		'name' => __('After Content', 'wp-bootstrap'),
-		'id' => 'after-content',
-		'before_widget' => '',
-		'after_widget' => '',
-		'before_title' => '<h3>',
-		'after_title' => '</h3>',
-	));
-	register_sidebar(
-		array(
-			'name' => __('Footer Sidebar', 'wp-bootstrap'),
-			'id' => 'footer-sidebar',
-			'before_widget' => '',
-			'after_widget' => '',
-			'before_title' => '',
-			'after_title' => '',
-		)
-	);
+	if(get_option('header_sidebar')) {
+		register_sidebar(
+			array(
+				'name' => __('Header Sidebar', 'wp-bootstrap'),
+				'id' => 'header-sidebar',
+				'before_widget' => '',
+				'after_widget' => '',
+				'before_title' => '',
+				'after_title' => '',
+			)
+		);
+	}
+	if(get_option('left_sidebar')) {
+		register_sidebar(
+			array(
+				'name' => __('Left Sidebar', 'wp-bootstrap'),
+				'id' => 'left-sidebar',
+				'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+				'after_widget' => '</aside>',
+				'before_title' => '<div class="caption">',
+				'after_title' => '</div>',
+			)
+		);
+	}
+	if(get_option('right_sidebar')) {
+		register_sidebar(
+			array(
+				'name' => __('Right Sidebar', 'wp-bootstrap'),
+				'id' => 'right-sidebar',
+				'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+				'after_widget' => '</aside>',
+				'before_title' => '<div class="caption">',
+				'after_title' => '</div>',
+			)
+		);
+	}
+	if(get_option('before_content')) {
+		register_sidebar(
+			array(
+				'name' => __('Before Content', 'wp-bootstrap'),
+				'id' => 'before-content',
+				'before_widget' => '',
+				'after_widget' => '',
+				'before_title' => '<h3>',
+				'after_title' => '</h3>',
+			)
+		);
+	}
+	if(get_option('after_content')) {
+		register_sidebar(
+			array(
+				'name' => __('After Content', 'wp-bootstrap'),
+				'id' => 'after-content',
+				'before_widget' => '',
+				'after_widget' => '',
+				'before_title' => '<h3>',
+				'after_title' => '</h3>',
+			)
+		);
+	}
+	if(get_option('footer_sidebar')) {
+		register_sidebar(
+			array(
+				'name' => __('Footer Sidebar', 'wp-bootstrap'),
+				'id' => 'footer-sidebar',
+				'before_widget' => '',
+				'after_widget' => '',
+				'before_title' => '',
+				'after_title' => '',
+			)
+		);
+	}
 });
 
 
-
-
+// Register Menu`s
 add_action('after_setup_theme', function() {
-	register_nav_menus( array(
-		'top-menu' => __('Top Menu', 'wp-bootstrap'),
-		'main-menu' => __('Main Menu', 'wp-bootstrap'),
-		'left-sidebar-menu' => __('Left Sidebar Menu', 'wp-bootstrap'),
-		'right-sidebar-menu' => __('Right Sidebar Menu', 'wp-bootstrap'),
-		'footer-menu' => __('Footer Menu', 'wp-bootstrap'),
-	) );
+	$menu = array();
+	if(get_option('top_menu'))
+		$menu['top-menu'] = __('Top Menu', 'wp-bootstrap');
+
+	if(get_option('main_menu'))
+		$menu['main-menu'] = __('Main Menu', 'wp-bootstrap');
+
+	if(get_option('left_sidebar_menu'))
+		$menu['left-sidebar-menu'] = __('Left Sidebar Menu', 'wp-bootstrap');
+
+	if(get_option('right_sidebar_menu'))
+		$menu['right-sidebar-menu'] = __('Right Sidebar Menu', 'wp-bootstrap');
+
+	if(get_option('footer_menu'))
+		$menu['footer-menu'] = __('Footer Menu', 'wp-bootstrap');
+
+	register_nav_menus($menu);
 });
 
 
--- theme-options.php
+++ theme-options.php
@@ -8,4 +8,64 @@
  * @version 1.0
  */
 
+
+function wp_bootstrap_options() {
+
+	global $themename, $shortname, $options;
+	if (!current_user_can('manage_options'))
+		wp_die('You do not have sufficient permissions to access this page.');
+
+	if($_REQUEST['action'] == 'save')
+		echo '<div class="notice notice-success inline wp-pp-notice"><p>Настройки темы <strong>'. $themename .'</strong> были успешно сохранены!</p></div>';
+
+	if($_REQUEST['reset'])
+		echo '<div class="notice notice-error inline wp-pp-notice"><p>Настройки темы <strong>'. $themename .'</strong> были сброшены!</p></div>';
+
+	if($_REQUEST['restore'])
+		echo '<div class="notice notice-warning inline wp-pp-notice"><p>Настройки темы <strong>'. $themename .'</strong> были восстановлены!</p></div>';
+
+}
+
+// Only admin actions
+if (is_admin()) {
+	add_action('admin_init', function() {
+
+		// Global html-code for head/body section
+		register_setting('wp-bootstrap-options', 'header_code');
+		register_setting('wp-bootstrap-options', 'footer_code');
+
+		// Register setting for sidebar`s visibility
+		register_setting('wp-bootstrap-options', 'header_sidebar');
+		register_setting('wp-bootstrap-options', 'left_sidebar');
+		register_setting('wp-bootstrap-options', 'right_sidebar');
+		register_setting('wp-bootstrap-options', 'before_content');
+		register_setting('wp-bootstrap-options', 'after_content');
+		register_setting('wp-bootstrap-options', 'footer_sidebar');
+
+		// Register setting for menu`s visibility
+		register_setting('wp-bootstrap-options', 'top_menu');
+		register_setting('wp-bootstrap-options', 'main_menu');
+		register_setting('wp-bootstrap-options', 'left_sidebar_menu');
+		register_setting('wp-bootstrap-options', 'right_sidebar_menu');
+		register_setting('wp-bootstrap-options', 'footer_menu');
+
+	});
+
+	add_action('admin_menu', function() {
+		add_menu_page(
+			__('WP Bootstrap Theme options', 'wp-bootstrap'),
+			__('Theme options', 'wp-bootstrap'),
+			'manage_options',
+			'theme-options',
+			'wp_bootstrap_options',
+			'dashicons-welcome-widgets-menus',
+			62 // Default position order for appearance menu + 2
+		);
+    });
+
+}
+
+
+
+
 ?>
