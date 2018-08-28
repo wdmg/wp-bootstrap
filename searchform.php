2ea1821b4b43d8ef1871a43198905133fea944c2
alex-wdmg <wdmg.com.ua@gmail.com>
2018-08-28 20:08:37 +0300

    CSS classes for search form

--- searchform.php
+++ searchform.php
@@ -9,7 +9,7 @@
  */
 
 ?>
-<form action="/" method="get" class="form-inline mt-2 mt-md-0">
+<form action="/" method="get" class="form-inline mt-2 mt-md-0 ml-auto mr-0">
 	<input id="search" type="search" name="s" class="form-control mr-sm-2" type="text" value="<?php the_search_query(); ?>" placeholder="Search" aria-label="Search">
 	<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 </form>
