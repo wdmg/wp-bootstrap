c080ce06fcc9fba78e14c1895ab6a2e5d5c32553
alex-wdmg <wdmg.com.ua@gmail.com>
2018-08-30 16:22:52 +0300

    Nav walker fix: added custom args for li.class and a.class of menu item

--- inc/nav-walker.php
+++ inc/nav-walker.php
@@ -24,29 +24,30 @@ class nav_walker extends Walker_Nav_menu {
 
 		$id = apply_filters('nav_menu_item_id', 'menu-item-'.$item->ID, $item, $args);
 		$id = strlen($id) ? ' id="'.esc_attr($id).'"' : '';
-
 		$indent = ($depth) ? str_repeat("\t", $depth) : '';
-		$is_active = ($item->current || $item->current_item_anchestor) ? ' active' : '';
-
-		$classes = empty($item->classes) ? array() : (array) $item->classes;
-		$classes[] = ($args->walker->has_children) ? 'dropdown' : '';
-		$classes[] = 'nav-item';
-		$classes[] = 'nav-item-'.$item->ID;
-
+		
+		$item_classes = empty($args->item_class) ? array('nav-item', 'nav-item-'.$item->ID) : (array) $args->item_class;
+		$item_classes[] = ($args->walker->has_children) ? 'dropdown' : '';
 		if($depth && $args->walker->has_children) {
-			$classes[] = 'dropdown-menu';
+			$item_classes[] = 'dropdown-menu';
 		}
-		$class_names =  join(' ', apply_filters('nav_menu_css_class', array_filter($classes), $item, $args));
-		$class_names = ' class="'.esc_attr($class_names).'"';
+		$item_classes =  array_merge($item->classes, $item_classes);
+		$item_classes =  join(' ', apply_filters('nav_menu_css_class', array_filter($item_classes), $item, $args));
+		$item_classes = ' class="'.esc_attr($item_classes).'"';
+		
+		$link_classes = empty($args->link_class) ? array('nav-link', 'nav-link-'.$item->ID) : (array) $args->link_class;
+		$link_classes[] = ($item->current || $item->current_item_anchestor) ? ' active' : '';
+		$link_classes =  join(' ', $link_classes);
+		$link_classes = ($args->walker->has_children) ? ' class="'.esc_attr($item_classes).' dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" ' : ' class="'.esc_attr($link_classes).'" ';
 
-		$output .= $indent.'<li '.$id.$value.$class_names.$item_attributes.'>';
+		$output .= $indent.'<li '.$id.$value.$item_classes.$item_attributes.'>';
 
-		$attributes = !empty( $item->attr_title) ? ' title="'.esc_attr($item->attr_title).'"' : '';
-		$attributes .= !empty( $item->target) ? ' target="'.esc_attr($item->target).'"' : '';
-		$attributes .= !empty( $item->xfn) ? ' rel="'.esc_attr($item->xfn).'"' : '';
-		$attributes .= !empty( $item->url) ? ' href="'.esc_attr($item->url).'"' : '';
-		$attributes .= ($args->walker->has_children) ? ' class="nav-link dropdown-toggle'.$is_active.'" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"' : ' class="nav-link'.$is_active.'"';
-
+		$attributes = !empty($item->attr_title) ? ' title="'.esc_attr($item->attr_title).'"' : '';
+		$attributes .= !empty($item->target) ? ' target="'.esc_attr($item->target).'"' : '';
+		$attributes .= !empty($item->xfn) ? ' rel="'.esc_attr($item->xfn).'"' : '';
+		$attributes .= !empty($item->url) ? ' href="'.esc_attr($item->url).'"' : '';
+		$attributes .= $link_classes;
+		
 		$item_output = $args->before;
 		$item_output .= ($depth > 0) ? '<a class="dropdown-item"'.$attributes.'>' : '<a'.$attributes.'>';
 		$item_output .= $args->link_before.apply_filters('the_title', $item->title, $item->ID).$args->link_after;
